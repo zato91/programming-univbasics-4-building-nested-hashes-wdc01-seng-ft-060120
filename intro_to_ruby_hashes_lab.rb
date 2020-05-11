c6ca83005c0d87ba3c1d6abb71dd1cdbbb4cea30
zara ahmadi <zatokey@gmail.com>
2020-05-11 17:36:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,22 +67,16 @@ def monopoly_with_fourth_tier
     
     :names => 
     {
-        :reading_railroad=>
-      {
-        :mortgage_value =>100
-      },
+      :reading_railroad=>
+      { :mortgage_value =>100 },
+      
       :pennsylvania_railroad=>
-      {
-        :mortgage_value =>200
-      },
+      {:mortgage_value =>200 },
+      
       :b_and_o_railroad=>
-      {
-        :mortgage_value =>400
-      },
-      :shortline_railroad =>
-      {
-        :mortgage_value =>800
-      }
+      {:mortgage_value =>400},
+      
+      :shortline_railroad =>{:mortgage_value =>800}
     }
   }
 }
