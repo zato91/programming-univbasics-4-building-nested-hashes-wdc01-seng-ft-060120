63e28c43dd5442b5ad1cd0029de87a089307a9cc
zara ahmadi <zatokey@gmail.com>
2020-05-11 17:30:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,8 +59,8 @@ def monopoly_with_fourth_tier
     
     :rent_in_dollars => 
     {
-      :one_piece_owne =>25,
-      :two_pieces_owne => 50,
+      :one_piece_owned =>25,
+      :two_pieces_owned => 50,
        :three_pieces_owned => 100, 
        :four_pieces_owned => 200,
     },
