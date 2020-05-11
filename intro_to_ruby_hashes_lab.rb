3965f3312f8c80582cd1730b94e2fa180da6aceb
zara ahmadi <zatokey@gmail.com>
2020-05-11 17:29:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,8 @@ def monopoly_with_third_tier
   :railroads => {
     :pieces => 4,
     :rent_in_dollars => {
-      :one_piece_owne =>25,
-      :two_pieces_owne => 50,
+      :one_piece_owned =>25,
+      :two_pieces_owned => 50,
        :three_pieces_owned =>100, 
        :four_pieces_owned => 200
     },
