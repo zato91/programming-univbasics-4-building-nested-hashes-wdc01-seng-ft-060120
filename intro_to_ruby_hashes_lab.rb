7bddf3370d6e4e8c7fae87a3f58d0d0f2609176b
zara ahmadi <zatokey@gmail.com>
2020-05-11 17:03:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,15 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+    railroads = Hash.new
+  railroads = {
+    :pieces => '4',
+    :rent_in_dollars => {
+      :in
+    },
+    :names => {},
+  }
+
 
 end
 
